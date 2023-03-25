b46ca7f89655b8ff002f8b3cb963fdd8cc5bfd30
Alicia MacEwan-Gutierrez <ali@Alicias-MacBook-Air.local>
2023-03-25 14:18:23 -0700

    added variables

---
+++ checkers.js
@@ -0,0 +1,24 @@
+/*checker board*/
+const board = [
+    null, 0, null, 1, null, 2, null, 3,
+    4, null, 5, null, 6, null, 7, null,
+    null, 8, null, 9, null, 10, null, 11,
+    null, null, null, null, null, null, null, null, 
+    null, null, null, null, null, null, null, null, 
+    12, null, 13, null, 14, null, 15, null, 
+    null, 16, null, 17, null, 18, null, 19,
+    20, null, 21, null, 22, null, 23, null, 
+]
+
+const cells = document.querySelectorAll("td")
+let redsPieces = document.querySelectorAll("p")
+let blacksPieces = document.querySelectorAll("span")
+const redTurnText = document.querySelectorAll(".red-turn-text")
+const blackTurnText = document.querySelectorAll(".black-turn-text")
+const divider = document.querySelector("#divider")
+
+let turn = true;
+let redScore = 12;
+let blackSCore = 12;
+let playerPieces;
+
